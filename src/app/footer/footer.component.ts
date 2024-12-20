74690e192b6357e53ead2b4ad7763134420d8b12
balabaieva <balabaievaa@gmail.com>
2024-12-20 12:58:16 -0600

    structure components

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavComponent } from '../nav/nav.component';
+
+@Component({
+  selector: 'app-footer',
+  imports: [NavComponent],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+}
